5dafb845ccd3e0fbe17676a5dcaf271467cc8092
釘宮愼之介 <kgmyshin@kugimiyasuke-no-MacBook-Pro.local>
2015-06-29 17:36:19 +0900

    [add] add InmutableDataSample

---
+++ InmutableDataSample/src/main/java/kgmyshin/databindingsample/MainActivity.java
@@ -0,0 +1,20 @@
+package kgmyshin.databindingsample;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import kgmyshin.databindingsample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("shinnosuke", "kugimiya");
+        binding.setAdminUser(user);
+    }
+}
---
+++ InmutableDataSample/src/main/java/kgmyshin/databindingsample/User.java
@@ -0,0 +1,13 @@
+package kgmyshin.databindingsample;
+
+/**
+ * Created by kgmyshin on 15/06/29.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
